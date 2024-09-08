834a4e5225e0c9530a611aad727b2eca63e253c7
JadenSimon <simonjaden223@gmail.com>
2024-09-08 15:20:17 -0700

    oops

--- test/packages/pkg2/main.ts
+++ test/packages/pkg2/main.ts
@@ -2,5 +2,5 @@ import * as pkg1 from 'pkg1'
 import { test, expectEqual } from 'synapse:test'
 
 test('line count', () => {
-    expectEqual(pkg1.self.split('\n').length, 13)
+    expectEqual(pkg1.self.text.split('\n').length, 13)
 })
