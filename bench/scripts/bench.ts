257c040e9915fcc39b530ccdfa5769e2aebf1f39
JadenSimon <simonjaden223@gmail.com>
2024-09-18 18:42:48 -0700

    try aws

--- bench/scripts/bench.ts
+++ bench/scripts/bench.ts
@@ -5,13 +5,13 @@ export async function main() {
     const synapseCmd = process.env.SYNAPSE_CMD || 'synapse'
     const totalRuns = 25
     const cwd = path.resolve(process.cwd())
-    await runCommand(synapseCmd, ['compile'], cwd)
+    await runCommand(synapseCmd, ['compile', '--target', 'aws'], cwd)
 
     let total = 0
     for (let i = 0; i < totalRuns; i++) {
         await runCommand(synapseCmd, ['clear-cache', 'compile'], cwd)
         const start = performance.now()
-        await runCommand(synapseCmd, ['compile'], cwd)
+        await runCommand(synapseCmd, ['compile', '--target', 'aws'], cwd)
         const duration = (performance.now() - start)
         console.log(`Took ${Math.round(duration)}ms`)
         total += duration
