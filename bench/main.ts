86db4c537165c3ca172eaf8aee060a5e07daefd6
JadenSimon <simonjaden223@gmail.com>
2024-09-18 18:38:49 -0700

    HMMMM

--- bench/main.ts
+++ bench/main.ts
@@ -1,8 +1,6 @@
 import { Function } from 'synapse:srl/compute'
 import { expect } from 'synapse:test'
 
-new Function(() => { expect(1 == 1) })
-new Function(() => { expect(2 == 2) })
-new Function(() => { expect(3 == 3) })
-new Function(() => { expect(4 == 4) })
-new Function(() => { expect(5 == 5) })
+for (let i = 0; i < 50; i++) {
+    new Function(() => { expect(i == i) })
+}
